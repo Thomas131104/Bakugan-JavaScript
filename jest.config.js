dbab485e0619a0b33c25e6d733f1d928f6f01353
Thomas131104 <periodgottagotell193972@gmail.com>
2025-03-22 15:49:12 +0700

    Clean up repository: remove ignored files and folders

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    testEnvironment: "node", // Đảm bảo Jest chạy trong môi trường Node.js
-    transform: {
-      "^.+\\.js$": "babel-jest", // Sử dụng Babel để transpile các file `.js`
-    },
-  };
-  
